47ce913ead20d0e3a2ccd9b1fad8b667d585ac53
Shubham Patel <shubhamashishpatel@gmail.com>
2022-10-20 20:25:47 +0530

    Create payDirectly.sol

---
+++ payDirectly.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract SendEther {
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    function sendEth(address payable _to) public payable {
+        // Just forward the ETH received in this payable function
+        // to the given address
+        uint amountToSend = msg.value;
+        // call returns a bool value specifying success or failure
+        (bool success, bytes memory data) = _to.call{value: msg.value}("");
+        require(success == true, "Failed to send ETH");
+    }
+}
